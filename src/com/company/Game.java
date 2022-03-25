00e54be17da4753fc1f1443b1199e2df9ea37090
Milen Georgiev <milegeorgiev@tu-sofia.bg>
2022-03-25 12:11:53 +0200

    Initial commit for goalie refactoring project

---
+++ src/com/company/Game.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Game {
+    private final double minutesPlayed;
+    private final int goalsAgainst;
+    private final int shotsOnGoalAgainst;
+
+    public Game(int goalsAgainst, int shotsOnGoalAgainst, double minutesPlayed) {
+        this.goalsAgainst = goalsAgainst;
+        this.shotsOnGoalAgainst = shotsOnGoalAgainst;
+        this.minutesPlayed = minutesPlayed;
+    }
+
+    public int getGoalsAgainst() {
+        return goalsAgainst;
+    }
+
+    public int getShotsOnGoalAgainst() {
+        return shotsOnGoalAgainst;
+    }
+
+    public double getMinutesPlayed() {
+        return minutesPlayed;
+    }
+}
